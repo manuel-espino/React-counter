2cf7d5e5a7e546350a6169b0fe35b6ccdcc32f01
Manuel Espino <manudrago@gmail.com>
2019-10-14 01:40:13 +0200

    WIP basic features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const updateCounter = (operation) => {
+    switch (operation) {
+      case 1:
+        setCount(count + step);
+        break;
+      case 2:
+        setCount(count - step);
+        break;
+      default:
+        setCount(count);
+    }
+  }
+
+  const fixStep = (e) =>{
+    const value = parseInt(e.target.value, 10);
+    setStep(value);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="counter">
+        <h2>Counter</h2>
+        <p className="counter__output">{count}</p>
+        <div className="counter__buttons">
+          <button className="counter__button counter__button--plus" onClick={() => updateCounter(1)}>+{step}</button>
+          <button className="counter__button counter__button--minus" onClick={() => updateCounter(2)}>-{step}</button>
+        </div>
+        <label className="counter__label">Step:</label>
+        <input className="counter__input" type="number" onChange={fixStep} value={step}/>
+      </div>
     </div>
   );
 }
